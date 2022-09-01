5f5cc37babada0d49bae7bb0da88d72e326fa74a
Torimune <71534350+Torimune29@users.noreply.github.com>
2022-09-01 18:39:13 +0900

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -105,9 +105,11 @@ log_note "[bootstrap start]"
 log_note "prerequisite package instal start"
 
 # install packages for chezmoi
+set +e
 eval $(get_package_management_method)
-set +e; eval $(get_escalation_method) $_facts_update 1>/dev/null ; set -e
+eval $(get_escalation_method) $_facts_update 1>/dev/null
 eval $(get_escalation_method) $_facts_install git curl tar bash 1>/dev/null
+set -e
 
 log_success "prerequisite package instal finished."
 log_note "init scripts start"
