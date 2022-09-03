73604b25c18c9f1e30c3fefe0f20ac9333369993
Torimune29 <>
2022-09-04 00:16:38 +0900

    impl diag for .profile

--- run_once_19_diagnosis.sh
+++ run_once_19_diagnosis.sh
@@ -29,18 +29,28 @@ declare -A validator=(
 
 print_script_info
 
+# validate cli tools
+log_note "validating cli tools"
 for i in "${!validator[@]}"
 do
   return_value=1
   eval "${validator[$i]}" > /dev/null 2>&1
   return_value=$?
-  if [ $return_value = 0 ]; then
-    log_success "$i"
-  else
+  if [ $return_value != 0 ]; then
     log_error "$i"
   fi
   unset return_value
 done
+log_success "validating cli tools done."
+
+# validate shell profile
+log_note "validating .profile on interactive"
+/bin/bash -i -c "set -e; . $HOME/.profile" > /dev/null
+if [ $? != 0 ]; then
+  log_error ".profile"
+  exit 1
+fi
+log_success "validating .profile on interactive done."
 
 echo ""
 log_success "$0 done."
